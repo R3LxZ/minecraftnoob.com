c0f2bff7530c5f46e636d0bb69da1dbc67c54bec
R3LxZ <108008695+R3LxZ@users.noreply.github.com>
2022-06-22 22:00:38 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,73 @@
+<head>
+
+       <title>Hacked by Fr3LL ft My Friends</title>
+
+<meta property="og:image"content="https://i.ibb.co/yR9FgdC/20211214-055009.jpg">
+
+<meta name="author" content="Me Mr.lonely"/>
+
+<meta name="copyright" content="2021"/>
+
+<meta name="title" content="Hacked By Mr.lonely">
+
+    <meta name="description" content="We are party on your site.">
+
+    </head>
+
+<link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
+
+ <body bgcolor="black">
+
+ <table width="100%" height="100%">
+
+ <td align="center">
+
+         <style type="text/css">
+
+         .x{
+
+         font-size:60px;
+
+         transform:skewY(-8deg);
+
+         }
+
+         </style>
+
+<font color="white" face="Space Mono">
+
+                            <h1 class="x">Hacked by<font color="white"> Fr3LL x MoonD4Y7 x R4KH4 x iCr4sh</h1>
+
+<font color="white">
+
+                            <h2>
+
+                        MSG: Patch your system idiot! </h2>
+
+					        <h3></h3>
+
+					        <!DOCTYPE html>
+
+					        <html lang="in">
+
+					        <head>
+
+					        				<title>nothing</title>
+
+					        </head>
+
+					        <body>
+
+					        				
+
+					        </body>
+
+					       
+
+				        
+
+			<body onclick="playAudio();" class=""><audio id="sec" __idm_id__="823202817"> 
+
+<source src="https://f.top4top.io/m_23627ph1o0.mp3" type="audio/mpeg"> 		</audio> <div class="xixixi"> <script> 	var x = document.getElementById("sec"); 	function playAudio() { 	x.play(); 	} 	 	function myFunction() { 		 var element = document.body; element.classList.toggle("dark-mode"); } function changeImage() { if (document.getElementById("imgClickAndChange").src == "body") { document.getElementById("imgClickAndChange").src = "body"; } else { document.getElementById("imgClickAndChange").src = "body"; } } </script> <center><br><br><br><p></p><blink>
+
+<style><style>
